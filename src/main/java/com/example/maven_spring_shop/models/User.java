f9d232616bf4b2ead1a0ba796e25d68258ded29a
Nguyễn Thanh Trung <thanhtruongba2001@gmail.com>
2023-03-14 16:47:23 +0700

    add model

---
+++ src/main/java/com/example/maven_spring_shop/models/User.java
@@ -0,0 +1,59 @@
+package com.example.maven_spring_shop.models;
+
+import jakarta.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private Boolean role;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getRole() {
+        return role;
+    }
+
+    public void setRole(Boolean role) {
+        this.role = role;
+    }
+}
